f0981eeb59a622f4164c46f827abd9fb009515be
Aleksey Spirkin <aleksey.spirkin@gmail.com>
2021-03-01 23:27:13 +0300

    First commit

---
+++ note-taking-web-app/src/test/java/com/aspirkin/notes/NoteTakingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspirkin.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
